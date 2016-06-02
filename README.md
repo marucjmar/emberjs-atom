afb051db3d2597ed227a5d0b29889700a40b0753
marucjmar18 <marcinelle1995@gmail.com>
2016-06-02 04:31:01 +0200

    The build structure

---
+++ README.md
@@ -0,0 +1,6 @@
+# Atom API Autocomplete Package [![Build Status](https://travis-ci.org/atom/autocomplete-atom-api.svg?branch=master)](https://travis-ci.org/atom/autocomplete-atom-api)
+
+Provides autocompletions for properties and methods available from the
+`atom.` global.
+
+![autocomplete-atom-api](https://cloud.githubusercontent.com/assets/69169/7211322/9c402ea2-e50e-11e4-9d74-56ab91aa101d.gif)
---
+++ routes/asd.js
@@ -0,0 +1,13 @@
+var methods = []
+$("#methods").children().each(function(object){
+    var name = $(this).find('code').first().text().replace(/[^\w\s]/gi, '');
+    var args = $(this).find('.args').first().text().split(',');
+    var textsnipet = name +'('+args.map(function(value, index){ return '${'+(index+1) + ':'+value.replace(/[^\w\s]/gi, '')+'}'})+')';
+    var descr = $(this).find('.description p').first().text().replace(/[^\w\s]/gi, '')
+    var link = 'http://emberjs.com/api/classes/Ember.computed.html#'+ $(this).attr('id');
+     methods.push({"name":name,"snippet":textsnipet,"description":descr,"descriptionMoreURL":link ,"leftLabel":name,"type":"property"})
+})
+
+JSON.stringify(methods)
+
+asd: Ember.computed.alias('asd')
